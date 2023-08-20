94c2db943174a31662a31fe58bd48827bf06873a
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 04:17:54 -0700

    practice complete

--- JavaScript/class 2/script.js
+++ JavaScript/class 2/script.js
@@ -28,18 +28,18 @@
 
 
 // Practice 5
-var num1 = +prompt("Enter First Number")
-var num2 = +prompt("Enter Second Number")
-var add = num1 + num2;
-var sub = num1 - num2;
-var mul = num1 * num2;
-var div = num1 / num2;
+// var num1 = +prompt("Enter First Number")
+// var num2 = +prompt("Enter Second Number")
+// var add = num1 + num2;
+// var sub = num1 - num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
 
- document.write("The sum of "+num1+" and "+num2+" is "+add)
- document.write("<br>")
- document.write("The subtraction of "+num1+" and "+num2+" is "+sub)
- document.write("<br>")
- document.write("The multiplication of "+num1+" and "+num2+" is "+mul)
- document.write("<br>")
- document.write("The division of "+num1+" and "+num2+" is "+div)
- document.write("<br>")
+//  document.write("The sum of "+num1+" and "+num2+" is "+add)
+//  document.write("<br>")
+//  document.write("The subtraction of "+num1+" and "+num2+" is "+sub)
+//  document.write("<br>")
+//  document.write("The multiplication of "+num1+" and "+num2+" is "+mul)
+//  document.write("<br>")
+//  document.write("The division of "+num1+" and "+num2+" is "+div)
+//  document.write("<br>")
