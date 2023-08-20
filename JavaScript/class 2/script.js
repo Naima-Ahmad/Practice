780904db274a4888dcb69e1e2e6cd7bcf00cf6f3
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 03:48:18 -0700

    update file

--- JavaScript/class 2/script.js
+++ JavaScript/class 2/script.js
@@ -38,4 +38,4 @@ var div = num1 / num2;
  document.write("The sum of "+num1+" and "+num2+" is "+add)
  document.write("The division of "+num1+" and "+num2+" is "+div)
  document.write("The multiplication of "+num1+" and "+num2+" is "+mul)
- document.write("The sum of "+num1+" and "+num2+" is "+add)
+ document.write("The subtraction of "+num1+" and "+num2+" is "+add)
