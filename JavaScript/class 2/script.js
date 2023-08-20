46b4d9073fe76ea4471214ce0759262cd58b1c29
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 03:25:51 -0700

    file update

--- JavaScript/class 2/script.js
+++ JavaScript/class 2/script.js
@@ -20,5 +20,8 @@
 
 var num1 = +prompt("Enter First Number")
 var num2 = +prompt("Enter Second Number")
+var add = num1+num2;
+var sub = num1-num2;
 
-document.write(num1+num2)
+document.write(add)
+document.write(sub)
