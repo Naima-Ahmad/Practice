ad4c1939ac33fa89c9fa37947a4f7d65650cd994
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 01:49:02 -0700

    update

--- JavaScript/class 2/script.js
+++ JavaScript/class 2/script.js
@@ -3,6 +3,16 @@
 
 // document.write(num1+num2)
 
+// var num1 = 10;
+// var num2 = 5;
+
+// document.write(num1-num2)
+
+// var num1 = 10;
+// var num2 = 5;
+
+// document.write(num1*num2)
+
 var num1 = 10;
 var num2 = 5;
 
