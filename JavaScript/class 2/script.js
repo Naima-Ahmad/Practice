5705a294a5b81fc777030a5ede8803dbe43235b7
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 03:03:35 -0700

    add variable

--- JavaScript/class 2/script.js
+++ JavaScript/class 2/script.js
@@ -13,7 +13,9 @@
 
 // document.write(num1*num2)
 
-var num1 = 10;
-var num2 = 5;
+// var num1 = 10;
+// var num2 = 5;
 
-document.write(num1/num2)
+// document.write(num1/num2)
+
+var num1 = prompt("Enter First Number")
