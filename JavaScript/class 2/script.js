9234404c52b2af4c8da1a7325edcedf21768c1ee
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 03:40:05 -0700

    file update

--- JavaScript/class 2/script.js
+++ JavaScript/class 2/script.js
@@ -1,28 +1,39 @@
+//practice 1
+
 // var num1 = 10;
 // var num2 = 5;
 
 // document.write(num1+num2)
+
+//practice 2
 
 // var num1 = 10;
 // var num2 = 5;
 
 // document.write(num1-num2)
 
+//practice 3
+
 // var num1 = 10;
 // var num2 = 5;
 
 // document.write(num1*num2)
+
+//practice 4
 
 // var num1 = 10;
 // var num2 = 5;
 
 // document.write(num1/num2)
 
+
+// Practice 5
 var num1 = +prompt("Enter First Number")
 var num2 = +prompt("Enter Second Number")
-var add = num1+num2;
-var sub = num1-num2;
-var mul = num1*num2;
-var div = num1/num2;
+var add = num1 + num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
 
-document.write("The sum of "+num1+" and "+num2+" is "+add+)
+ document.write("The sum of "+num1+" and "+num2+" is "+add)
+// document.write("The sum of "+num1+" and "+num2+" is "+add+)
