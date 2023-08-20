8921e52ff83916a31d2b8fe843bafcf65e664951
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-20 04:31:41 -0700

    class 2 practice

--- practice/javascript/class 2/script.js
+++ practice/javascript/class 2/script.js
@@ -14,17 +14,17 @@
 
 //  document.write(num1-num2)
 
-// // practice 3
+// practice 3
 // var num1 = 15;
 // var num2 = 10;
 // document.write(num1 * num2)
 
-// // Practice 4
+// Practice 4
 // var num1 = 15;
 // var num2 = 10;
 // document.write(num1 * num2)
 
-// // Practice 5
+// Practice 5
 // var num1 = 15;
 // var num2 = 10;
 // document.write(num1 * num2)
