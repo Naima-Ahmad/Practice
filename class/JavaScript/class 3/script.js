450ab84a57008736d1147fd0f9ca4231525895de
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-23 05:41:43 -0700

    class 3 practice

--- class/JavaScript/class 3/script.js
+++ class/JavaScript/class 3/script.js
@@ -1,5 +1,10 @@
 var num = 76;
 
-if(num == 76){
-    document.write("Correct")
-}
+// if(num == 76){
+//     document.write('Correct')
+// }
+
+
+// if(num == 76){
+//     document.write('Correct')
+// }
