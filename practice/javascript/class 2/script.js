1f6799b25152b9ea460eaedfe60538f69efff5f4
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-23 03:35:42 -0700

    commit selected area

--- practice/javascript/class 2/script.js
+++ practice/javascript/class 2/script.js
@@ -43,10 +43,10 @@ var sub = num1-num2;
 var mul = num1*num2;
 var div = num1/num2;
 
-document.write("The sum of "+num1+" and "+num2+" is "+add)
-document.write("<br>")
-document.write("The subtraction of "+num1+" and "+num2+" is "+sub)
-document.write("<br>")
-document.write("The multiplication of "+num1+" and "+num2+" is "+mul)
-document.write("<br>")
-document.write("The division of "+num1+" and "+num2+" is "+div)
+// document.write("The sum of "+num1+" and "+num2+" is "+add)
+// document.write("<br>")
+// document.write("The subtraction of "+num1+" and "+num2+" is "+sub)
+// document.write("<br>")
+// document.write("The multiplication of "+num1+" and "+num2+" is "+mul)
+// document.write("<br>")
+// document.write("The division of "+num1+" and "+num2+" is "+div)
