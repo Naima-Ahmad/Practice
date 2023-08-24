e2c9f134c9c760ecff50a471fdf102c3214d34d6
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-23 18:52:53 -0700

    class 3 practice

--- practice/javascript/class 3/script.js
+++ practice/javascript/class 3/script.js
@@ -52,8 +52,7 @@ else if (perc >= 70 && perc < 80) {
 else if (perc >= 60 && perc < 70) {
     document.write("Your grade is C")
 }
-else if (perc >= 50 && perc < 60) {
-}
+else if (perc >= 50 && perc < 60) 
 
 
 
