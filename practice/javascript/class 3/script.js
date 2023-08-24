540d2d6ea53d16c3319aaf82d5a982b15c226e35
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-23 18:00:19 -0700

    class 3 practice

--- practice/javascript/class 3/script.js
+++ practice/javascript/class 3/script.js
@@ -1,26 +1,26 @@
 // Practice 1
 
-var perc = prompt("Enter your Percentage");
+// var perc = prompt("Enter your Percentage");
 
-if(perc >= 90 && perc <101){
-    document.write("Your grade is A+")
-}
+// if(perc >= 90 && perc <101){
+//     document.write("Your grade is A+")
+// }
 
-else if (perc >=80 && perc <90){
-    document.write("Your grade is A")
-}
+// else if (perc >=80 && perc <90){
+//     document.write("Your grade is A")
+// }
 
-else if (perc >=70 && perc <80){
-    document.write("Your grade is B")
-}
+// else if (perc >=70 && perc <80){
+//     document.write("Your grade is B")
+// }
 
-else if (perc >=60 && perc <70){
-    document.write("Your grade is C")
-}
-else if (perc >=50 && perc <60){
-    document.write("Your grade is D")
-}
+// else if (perc >=60 && perc <70){
+//     document.write("Your grade is C")
+// }
+// else if (perc >=50 && perc <60){
+//     document.write("Your grade is D")
+// }
 
-else{
-    document.write("Your grade is F")
-}
+// else{
+//     document.write("Your grade is F")
+// }
