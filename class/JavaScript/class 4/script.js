8d57367eb898c160451df15c4a0facc8e63f5ee4
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-25 08:55:17 -0700

    class 4 practice

--- class/JavaScript/class 4/script.js
+++ class/JavaScript/class 4/script.js
@@ -1,8 +1,9 @@
 // Example 1
-var firstName = "Muhammad";
-var middleName = "Ahmad";
-var lastName = "Nawaz";
-var age = "35";
-var roll = "0284";
+
+var firstName = "Muhammad"
+var middleName = "Ahmad"
+var lastName = "Nawaz"
+var age = "35"
+var roll = "0284"
 
 document.write(firstName+" "+middleName+" "+lastName)
