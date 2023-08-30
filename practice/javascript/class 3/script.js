d917b7ae61f8c221974420a71ea8ea6706ce2bdd
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-30 01:15:23 -0700

    class 3 practice

--- practice/javascript/class 3/script.js
+++ practice/javascript/class 3/script.js
@@ -55,6 +55,17 @@
         // }
         
         // Pracitce 3
+        var naam = prompt("Enter Your Name");
+        var eng = +prompt("Enter Your English Marks");
+        var urdu = +prompt("Enter Your Urdu Marks");
+        var math = +prompt("Enter Your Maths Marks");
+        var isl = +prompt("Enter Your Islamiyat Marks");
+        var sci = +prompt("Enter Your G.science Marks");
+
+        var obtain = nam+eng+urdu+math+isl+sci;
+        var perc = (obtain / 500) * 100;
+        var grade;
+        
         
         
         
