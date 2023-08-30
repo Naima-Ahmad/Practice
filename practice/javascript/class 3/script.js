54fd6942f0768ee7ff813e8f5cd78e7af7b9ae40
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-29 18:56:57 -0700

    class 3 practice

--- practice/javascript/class 3/script.js
+++ practice/javascript/class 3/script.js
@@ -33,13 +33,11 @@
         var maths = +prompt("Enter Maths Marks");
         var isl = +prompt("Enter Islamiat Marks");
         var sci = +prompt("Enter Science Marks");
-
+        
         var grade ;
         var perc = (total / 500) * 100;
-
-        document.write (grade);
-        document.write (perc);
-
+        
+        
         if(perc >= 90 && perc < 101){
             document.write("A+")
         }
@@ -47,39 +45,40 @@
         else if(perc >= 80 && perc < 90){
             document.write("A")
         }
-
+        
         else if(perc >= 70 && perc < 80){
             document.write("B")
         }
-
+        
         else if(perc >= 60 && perc < 70){
             document.write("C")
         }
-
+        
         else if(perc >= 50 && perc < 60){
             document.write("D")
         }
         else {
             document.write("F")
         }
-
+        
         document.write(perc)
         document.write(grade)
         
         
-
-
-
+        
+        
+        
 
     
-
-
-
-
-
-
-
-
-
-
-
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
