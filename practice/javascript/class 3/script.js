e26eb770189dcdb049754863f90b47d77eb056f5
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-08-29 19:03:11 -0700

    class 3 practice

--- practice/javascript/class 3/script.js
+++ practice/javascript/class 3/script.js
@@ -28,31 +28,31 @@
         
         // practice 2
        
-        var perc = +prompt("Enter Your Percentage");
+        // var perc = +prompt("Enter Your Percentage");
         
         
-        if(perc >= 90 && perc < 101){
-            document.write("A+")
-        }
+        // if(perc >= 90 && perc < 101){
+        //     document.write("A+")
+        // }
 
-        else if(perc >= 80 && perc < 90){
-            document.write("A")
-        }
+        // else if(perc >= 80 && perc < 90){
+        //     document.write("A")
+        // }
         
-        else if(perc >= 70 && perc < 80){
-            document.write("B")
-        }
+        // else if(perc >= 70 && perc < 80){
+        //     document.write("B")
+        // }
         
-        else if(perc >= 60 && perc < 70){
-            document.write("C")
-        }
+        // else if(perc >= 60 && perc < 70){
+        //     document.write("C")
+        // }
         
-        else if(perc >= 50 && perc < 60){
-            document.write("D")
-        }
-        else {
-            document.write("You Are Fail")
-        }
+        // else if(perc >= 50 && perc < 60){
+        //     document.write("D")
+        // }
+        // else {
+        //     document.write("You Are Fail")
+        // }
         
         
         
