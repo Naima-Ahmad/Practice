182ac55b3109e67df746613bceeb0db9e5947831
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-01 16:45:16 -0700

    Class 4

--- class/JavaScript/class 4/script.js
+++ class/JavaScript/class 4/script.js
@@ -9,5 +9,33 @@
 
 // Example 2
 
-var arr = ["Naima","Ahmad","11 years old"];
-document.write(arr[1])
+// var arr = ["Naima","Ahmad","11 years old"];
+// document.write(arr[1])
+
+
+// Example 3
+
+// var arr = ["Naima","Ahmad","11"];
+// document.write(arr);
+
+
+// Example 4
+
+// var arr = ["Naima","Ahmad","11"];
+
+// arr.push("8","50")
+
+// document.write(arr);
+
+
+// Example 5
+
+// var arr = ["Naima","Ahmad","11","8","50"];
+// arr.pop()
+// document.write(arr);
+
+
+// Example 6
+var arr = ["Naima","Ahmad","11","8","50"];
+arr.pop()
+document.write(arr);
