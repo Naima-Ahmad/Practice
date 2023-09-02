c68f7873f6a98cded0c65703e7d0fea2d2516239
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-01 17:31:58 -0700

    class 4

--- class/JavaScript/class 4/script.js
+++ class/JavaScript/class 4/script.js
@@ -96,3 +96,8 @@
 var arr = ["Naima","Ahmad","11","8","50"];
 arr.splice(2,0,"Good Student")
 document.write(arr);
+
+
+var arr = ["Naima","Ahmad","11","8","50"];
+arr.splice(2,0,"Good Student")
+document.write(arr);
