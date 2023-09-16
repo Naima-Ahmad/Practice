a51fb62c8f6b7111e2ed93bbd40df18e41a8418e
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-15 19:07:35 -0700

    task

--- practice/javascript/class 4/script.js
+++ practice/javascript/class 4/script.js
@@ -3,9 +3,14 @@ var b = 3;
 var c = 4;
 
 var d = a++ + a++ + ++a + b-- - b-- + c++ + c + a++ + a + c-- + ++c;
-
+                                 
 
 var e = a++ + ++a + ++b +b + ++a - a++ - c-- - c-- - c++ + ++c + a++;
 
 
-var f = a++ - --c - c-- - b-- + --a + a++ - a++ - b++ + b++;
+var f = a++ - --c - c-- - b-- + --a + a++ - a++ - b++ + b++ - a++ + a++ + a;
+
+
+document.write(d+"<br>")
+document.write(e+"<br>")
+document.write(f+"<br>")
