4f30c8ca2b04f0a71830940c3512aa44c60cbac2
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-16 00:53:45 -0700

    class 5

--- class/JavaScript/class 5/script.js
+++ class/JavaScript/class 5/script.js
@@ -140,7 +140,7 @@
 // Example 11
 
  function tablegeneration(){
-     for(i=1, i <= 10; i++;){
+    //  for(i=1, i <= 10; i++;){
         //  document.write(2 + " X "+i+" = "+2*i+"<br>")
     //  }
 //  }
