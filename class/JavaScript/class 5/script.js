71384f79da80edc9996ed4480126a546752124a1
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-15 23:45:19 -0700

    class 5

--- class/JavaScript/class 5/script.js
+++ class/JavaScript/class 5/script.js
@@ -91,14 +91,14 @@
 
 // Example 7
 
-var player1 = prompt("Enter Heads Name");
-var player2 = prompt("Enter Tails Name");
-var toss = Math.random() * 2;
-var convert = Math.floor(toss);
+// var player1 = prompt("Enter Heads Name");
+// var player2 = prompt("Enter Tails Name");
+// var toss = Math.random() * 2;
+// var convert = Math.floor(toss);
 
-if(conver == 0){
-    document.write(player1+" Heads wins")
-}
-else{
-    document.write(player2+" Tails wins")
-}
+// if(convert == 0){
+//     document.write(player1+" Heads wins")
+// }
+// else{
+//     document.write(player2+" Tails wins")
+// }
