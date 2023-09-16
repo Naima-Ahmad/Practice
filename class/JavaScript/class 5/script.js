95c3bb6c3baed0e32fb2903fc07a2955a93ec8cb
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-15 22:34:59 -0700

    class 5

--- class/JavaScript/class 5/script.js
+++ class/JavaScript/class 5/script.js
@@ -1,5 +1,10 @@
-var arr = ["Naima","Laiba","Hooriya","Arsheya","Amna","Maryam","Saira"];
+// Example 1
 
-for(var i = 0; i < arr.length; i++){
-    document.write(arr[i]+"<br>")
-}
+// var arr = ["Naima","Laiba","Hooriya","Arsheya","Amna","Maryam","Saira"];
+
+// for(var i = 0; i < arr.length; i++){
+//     document.write(arr[i]+"<br>")
+// }
+
+
+// Example 2
