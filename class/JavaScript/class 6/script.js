69ab32dc2e73769e6c3e99c13e96e43f49ebca3c
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-17 06:03:52 +0500

    class 5

--- class/JavaScript/class 6/script.js
+++ class/JavaScript/class 6/script.js
@@ -1,3 +1,7 @@
+// Example 1
+
 function show(){
-    document.getElementById("p").innerHTML = ""
+    document.getElementById("p").innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, perspiciatis. Earum, nam quibusdam. Expedita"
 }
+
+// Example 2
