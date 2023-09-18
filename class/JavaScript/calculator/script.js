5cbcc3771ded493232b325207bf6b5bb43b9f61d
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-09-18 18:23:58 +0500

    calculator

--- class/JavaScript/calculator/script.js
+++ class/JavaScript/calculator/script.js
@@ -4,7 +4,13 @@ function abc(a){
 
 
 function eql(){
-    var a = documnet.getElementById("inpt");
-    a.value = eval(a.value);
+var a = documnet.getElementById("inpt");
+a.value = eval(a.value);
 }
+
+function eql(){
+var a = document.getElementById("inpt");
+a.value = eval(a.value);
+}
+   
   
