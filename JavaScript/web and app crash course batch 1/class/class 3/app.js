d8774452761beb4ea96174ee537c4fb48e833996
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-02 20:05:07 +0500

    class 3

--- JavaScript/web and app crash course batch 1/class/class 3/app.js
+++ JavaScript/web and app crash course batch 1/class/class 3/app.js
@@ -1,4 +1,4 @@
 // example 1
-document.write("hello world")
-alert("hello world")
-console.log("hello world")
+// document.write("hello world")
+// alert("hello world")
+// console.log("hello world")
