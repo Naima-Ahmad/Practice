1b5d71221b3caea4ba03a31ae706986ec1e63e01
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-07 06:45:29 +0500

    class 4

--- JavaScript/web and app crash course batch 1/class/class 3/app.js
+++ JavaScript/web and app crash course batch 1/class/class 3/app.js
@@ -53,9 +53,9 @@
 
 // document.write(d)
 
-var num1 = 10++;
-var num2 = ++10;
-var num3 = 10--;
-var num4 = --10;
+// var num1 = 10++;
+// var num2 = ++10;
+// var num3 = 10--;
+// var num4 = --10;
 
 
