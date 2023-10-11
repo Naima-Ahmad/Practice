6cb5a4ba72b9812cb56c8fd51aec11ddb3428470
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-11 06:38:44 +0500

    class 4

--- JavaScript/web and app crash course batch 1/class/class 4/app.js
+++ JavaScript/web and app crash course batch 1/class/class 4/app.js
@@ -1,4 +1,11 @@
 // Example 1
 
-var fname = prompt("Enter first Name");
-document.write(fname)
+// var fname = prompt("Enter first Name","Naima Ahmad");
+// document.write(fname)
+
+
+// Example 2
+
+var myNum = 5;
+var userNum = prompt("Enter a Number");
+document.write(myNum+userNum);
