725597339958e7e80ba13e70e29865f23687aaad
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-13 04:53:43 +0500

    class 5

--- JavaScript/web and app crash course batch 1/class/class 5/app.js
+++ JavaScript/web and app crash course batch 1/class/class 5/app.js
@@ -2,4 +2,7 @@
 
 var meriEmail = "admin";
 var meriPass = "admin";
-var userkaemail = prompt();
+var userkaemail = prompt("Enter your email ?");
+var userkaPass = prompt("Enter your Password ?");
+
+if(userkaemail === meriEmail && userkaPass === meriPass)
