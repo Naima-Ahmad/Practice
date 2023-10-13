2a4fa512d8d73b87a9fa40f6e199821d3f4abd35
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-13 13:04:14 +0500

    class 7

---
+++ JavaScript/web and app crash course batch 1/class/class 7/app.js
@@ -0,0 +1,17 @@
+// Example 1
+
+// var words = "Naima Ahmad";
+// document.write(words.toUpperCase())
+
+
+// Example 2
+
+// var abc = "ABCDEFGH";
+// document.write(abc.toLowerCase())
+
+
+// Example 3
+
+var abc = "Naima";
+var a = abc.slice(0,1)
+document.write(a)
