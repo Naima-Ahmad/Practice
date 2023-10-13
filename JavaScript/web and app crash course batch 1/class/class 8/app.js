0db33072c1630d5a1137767cde551b0bab615ddc
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-13 20:26:15 +0500

    class 8

--- JavaScript/web and app crash course batch 1/class/class 8/app.js
+++ JavaScript/web and app crash course batch 1/class/class 8/app.js
@@ -6,7 +6,45 @@
 
 // Example 2
 
-var abc = new Date();
-var b = abc.toString();
-var c = b.slice(0,3);
-document.write(c)
+// var abc = new Date();
+// var b = abc.toString();
+// var c = b.slice(0,3);
+// document.write(c)
+
+
+// Example 3
+
+// var abc = new Date();
+// var b = abc.toString();
+// var c = b.slice(0,6);
+// document.write(c)
+
+
+// Example 4
+
+// var abc = new Date();
+// var b = abc.toString();
+// var c = b.slice(0,7);
+// document.write(c)
+
+
+// Example 5
+
+// var a = new Date();
+// var b = a.getDay();
+// document.write(b)
+
+
+// Example 5
+
+// var a = new Date();
+// var b = a.getTime();
+// document.write(b);
+
+
+// Example 6
+
+// Age Calculator
+
+var dob = new Date("6 sep, 2011");
+var dobmili = dob.getTime   
