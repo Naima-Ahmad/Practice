5e526aafe399c4ab568e466d85108d9a26321693
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-13 06:26:58 +0500

    class 6

--- JavaScript/web and app crash course batch 1/class/class 6/app.js
+++ JavaScript/web and app crash course batch 1/class/class 6/app.js
@@ -1,4 +1,12 @@
-var a = 1;
-var b = 2;
-var c = 3;
-var d = 4;
+// Example 1
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// var d = 4;
+// document.write(a,b,c,d)
+
+
+// Example 2
+
+for(var i = 0; i <= 10;)
