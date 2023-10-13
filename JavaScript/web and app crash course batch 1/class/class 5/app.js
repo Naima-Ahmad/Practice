adceb191f7543261453d2faecb40e39bff52a840
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-13 05:10:06 +0500

    class 5

--- JavaScript/web and app crash course batch 1/class/class 5/app.js
+++ JavaScript/web and app crash course batch 1/class/class 5/app.js
@@ -5,6 +5,21 @@
 // var userkaemail = prompt("Enter your email ?");
 // var userkaPass = prompt("Enter your Password ?");
 
+// if(userkaemail === meriEmail || userkaPass === meriPass){
+//     document.write("you can go now --)")
+// }
+// else{
+//     document.write("Enter correct Email and Password")
+// }
+
+ 
+// Example 2
+
+// var meriEmail = "admin";
+// var meriPass = "admin";
+// var userkaemail = prompt("Enter your email ?");
+// var userkaPass = prompt("Enter your Password ?");
+
 // if(userkaemail === meriEmail && userkaPass === meriPass){
 //     document.write("you can go now --)")
 // }
@@ -13,16 +28,28 @@
 // }
 
 
-// Example 2
+// ARRAYS
+// Example 3
 
-var meriEmail = "admin";
-var meriPass = "admin";
-var userkaemail = prompt("Enter your email ?");
-var userkaPass = prompt("Enter your Password ?");
+// var fname = "Naima"
+// var lastname = "Laiba"
+// var age = 11
+// var uni = "smit"
 
-if(userkaemail === meriEmail || userkaPass === meriPass){
-    document.write("you can go now --)")
-}
-else{
-    document.write("Enter correct Email and Password")
-}
+
+// Example 4
+
+// var collection = ["Naima","Ahmad",11,"Smit",true,false];
+// document.write(collection[3])
+
+
+// Example 5
+
+// var collection = ["Naima","Ahmad",11,"Smit",true,false];
+// document.write(collection[0])
+
+
+// Example 6
+
+var collection = ["Naima","Ahmad",11,"Smit",true,false];
+document.write(collection)
