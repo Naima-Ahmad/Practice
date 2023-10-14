94a1f31680e73f9f17f6296f06fdb6136e026ed6
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-14 05:52:50 +0500

    class 9

---
+++ JavaScript/web and app crash course batch 1/class/class 9/app.js
@@ -0,0 +1,13 @@
+// Example 1
+
+// for (var i = 0; i<= 10; i++){
+//     document.write(i+"<br>")
+// }
+
+
+// Example 2
+
+// nested loops
+
+var arr = ["Naima","Ahmad"];
+var secondarr = ["Laiba"];
