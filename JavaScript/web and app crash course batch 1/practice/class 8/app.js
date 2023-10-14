7823befc600c45442d02ae80e3e72ef21afe8b22
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-10-14 05:05:18 +0500

    practicing

--- JavaScript/web and app crash course batch 1/practice/class 8/app.js
+++ JavaScript/web and app crash course batch 1/practice/class 8/app.js
@@ -1,9 +1,8 @@
-var xyz = prompt("Enter your birth date :")
-var abc = new Date(xyz);
-var dob = abc.getTime()
-var today = new Date ();
-var efg = today.getTime();
-var diff = efg - dob;
-var age = Math.floor(diff / (1000*60*60*24*30*12));
-
-document.write(age)
+var xyz = prompt("Enter your birth date :");
+var dob = new Date(xyz);
+var dobmili = dob.getTime();
+var today = new Date();
+var todaymili = today.getTime();
+var sub = todaymili - dobmili;
+var diff = sub/(1000*60*60*24*30*12)
+document.write(Math.floor(diff)); 
