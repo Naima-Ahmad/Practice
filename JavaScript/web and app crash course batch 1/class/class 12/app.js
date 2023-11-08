3df6e0ae1cfc8b83f551907505cda3e2a2ae9d16
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-08 18:58:07 +0500

    class 12

--- JavaScript/web and app crash course batch 1/class/class 12/app.js
+++ JavaScript/web and app crash course batch 1/class/class 12/app.js
@@ -52,4 +52,5 @@ function styleapply(){
     var a = document.getElementsByTagName('p');
     a[1].style.backgroundColor = 'green'
     a[1].style.color = 'yellow'
+    a[1].style.padding = '5px'
 }
