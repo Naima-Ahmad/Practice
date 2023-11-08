432495823a6d04c9f0a19c15fe77b36e4d0a0b8d
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-08 16:03:56 +0500

    class 12

--- JavaScript/web and app crash course batch 1/class/class 12/app.js
+++ JavaScript/web and app crash course batch 1/class/class 12/app.js
@@ -1,6 +1,10 @@
 // Example 1
 
-function readMore(){
-    var a = document.getElementById("abc");
-    a.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae culpa vero fugit expedita voluptates porro, architecto, dolorum nisi libero odio est. Accusantium ipsa cum autem accusamus facere deserunt omnis aspernatur?"
-}
+// function readMore(){
+//     var a = document.getElementById("abc");
+//     a.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae culpa vero fugit expedita voluptates porro, architecto, dolorum nisi libero odio est. Accusantium ipsa cum autem accusamus facere deserunt omnis aspernatur?"
+// }
+
+
+// Example 2
+
