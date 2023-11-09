595ed226b27d536b0e78db03a6730666549267e4
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-09 10:02:47 +0500

    task of class 12

--- JavaScript/web and app crash course batch 1/practice/class 12/app.js
+++ JavaScript/web and app crash course batch 1/practice/class 12/app.js
@@ -1,11 +1,4 @@
 function applycss(){
-    var a = document.getElementById('hw');
-    a.className='hello';
-    var b = document.getElementById('apply');
-    b.style.display= 'none';
-}
-
-function back(){
-    var c = document.getElementById('apply');
-    c.style.disple='inline';
+    var a = document.getElementById('bac');
+    a.style.display='none';
 }
