b3795a6dd6307ea6c1cde593cf240824afcaef72
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-09 17:31:19 +0500

    class 13

--- JavaScript/web and app crash course batch 1/class/class 13/app.js
+++ JavaScript/web and app crash course batch 1/class/class 13/app.js
@@ -1,10 +1,14 @@
 // Example 1
 
-function apcss(){
-    var a = document.getElementById('divmain').getElementsByTagName('p');
-    for(var i = 0; i<a.length; i++){
-        a[i].style.backgroundColor='gold'
-        a[i].style.color='yellow'
-        a[i].style.padding='5px'
-    }
-}
+// function apcss(){
+//     var a = document.getElementById('divmain').getElementsByTagName('p');
+//     for(var i = 0; i<a.length; i++){
+//         a[i].style.backgroundColor='gold'
+//         a[i].style.color='yellow'
+//         a[i].style.padding='5px'
+//     }
+// }
+
+
+// Example 2
+
