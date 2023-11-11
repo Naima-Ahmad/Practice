236355fd44980ff2277d4c5e69ef093237b60584
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-11 16:10:14 +0500

    class 14

--- JavaScript/web and app crash course batch 1/class/class 14/app.js
+++ JavaScript/web and app crash course batch 1/class/class 14/app.js
@@ -40,7 +40,7 @@
 
 // Example 6
 
-var CE = document.createElement('p')
-var b = document.createTextNode('hello world')
-CE.appendChild(b)
-console.log(CE)
+// var CE = document.createElement('p')
+// var b = document.createTextNode('hello world')
+// CE.appendChild(b)
+// console.log(CE)
