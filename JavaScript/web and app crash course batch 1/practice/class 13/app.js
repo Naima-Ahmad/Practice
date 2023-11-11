c7847f490b19c80f08740a595042a74d1b1d1758
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-11 05:31:54 +0500

    stop watch making practice

--- JavaScript/web and app crash course batch 1/practice/class 13/app.js
+++ JavaScript/web and app crash course batch 1/practice/class 13/app.js
@@ -1,39 +1,6 @@
 var min = 0
 var sec = 0
 var msec = 0
-var a = document.getElementById('msec')
+var a = document.getElementById('min')
 var b = document.getElementById('sec')
-var c = document.getElementById('min')
-var interval;
-
-function start(){
-    interval =  setInterval(function(){
-        msec++
-        a.innerHTML = msec;
-        if(msec >= 100){
-            sec++
-            b.innerHTML = sec
-            msec = 0
-        }
-        else if (sec >= 60){
-            min++
-            c.innerHTML = min
-            sec = 0
-        }
-    },10)
-    document.getElementById('start').disabled = true;
-}
-
-function stop(){
-    clearInterval(interval)
-    document.getElementById('start').disabled = false;
-}
-
-function reset(){
-    var min = 0
-    var sec = 0
-    var msec = 0
-    a.innerHTML = msec
-    b.innerHTML = sec
-    c.innerHTML = min
-}
+var c = document.getElementById('msec')
