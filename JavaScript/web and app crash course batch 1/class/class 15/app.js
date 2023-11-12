d95378b26156c5d01f7fd460ae3bf4f564621afd
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-11-12 06:55:25 +0500

    class 15

--- JavaScript/web and app crash course batch 1/class/class 15/app.js
+++ JavaScript/web and app crash course batch 1/class/class 15/app.js
@@ -1,7 +1,27 @@
 // Example 1
 
+// var obj = {
+//     name1 : 'Naima'
+// }
+
+// document.write(obj.name1)
+
+
+// Example 2
+
+// var obj = {
+//     name1 : 'Naima',
+//     age : 12
+// }
+
+// document.write(obj.age)
+
+
+// Example 3
+
 var obj = {
-    name1 : 'Naima'
+    name1 : 'Naima',
+    age : 12
 }
 
-document.write(obj)
+document.write(obj.age)
