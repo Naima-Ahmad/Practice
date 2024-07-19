ea04e6322ef5e0b5eea1f11f503729557d110591
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-19 22:02:51 +0500

    chapter 5

--- books/a smarter way to learn javascript/chapter 5/app.js
+++ books/a smarter way to learn javascript/chapter 5/app.js
@@ -1,2 +1,6 @@
 // Question 1
-document.write("The sum of 3 and 5 is ", 3+5);
+// document.write("The sum of 3 and 5 is ", 3+5);
+
+// Question 2
+document.write("The sub of 3 and 5 is ", 3-5);
+document.write("<br>")
