29d0430acd70871b5927f2eba8622873d3dc0f5e
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-19 21:57:22 +0500

    chapter 4

---
+++ books/a smarter way to learn javascript/chapter 4/app.js
@@ -0,0 +1,31 @@
+// Question 1
+// var userName age school;
+
+// Question 2
+// legal variables
+// var myName;
+// var your_name;
+// var num1;
+// var friend1phone;
+// var End;
+// // illegal variables
+// var my Name;
+// var your-name;
+// var 1num;
+// var -friend1phone;
+// var var;
+
+// Question 3
+var heading = "Rules for naming JS variables";
+var st1 = "Variable names can only contain numbers, $ and _ . For example: $my_1stVariable";
+var st2 = "Variable must begin with a letter, $ or _ . For example: $name, _name or name";
+var st3 = "Variable names are case sensitive";
+var st4 = "Variable names should not be JS keywords";
+document.write("<h1>"+heading+"</h1>");
+document.write(st1);
+document.write("<br>");
+document.write(st2);
+document.write("<br>");
+document.write(st3);
+document.write("<br>");
+document.write(st4);
