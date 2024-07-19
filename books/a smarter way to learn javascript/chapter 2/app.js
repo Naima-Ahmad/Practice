c6f66809041656628d6bf4fd8c0f48b364ba18e7
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-19 20:51:21 +0500

    chapter 2 practice

---
+++ books/a smarter way to learn javascript/chapter 2/app.js
@@ -0,0 +1,37 @@
+// Question 1
+var userName;
+
+// Question 2
+var myName = "Naima Ahmad";
+
+// Question 3
+var message;
+message = "Hello World!";
+alert(message);
+
+// Question 4
+var student = "Hammad";
+var age = "12 years old";
+var courseApplied = "Techno kids";
+alert(student);
+alert(age);
+alert(courseApplied);
+
+// Question 5
+var item = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(item)
+
+// Question 6
+var email = "example@example.com";
+alert("My email address is " + email);
+
+// Question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Question 8
+document.write("Yah! I can write HTML content throught JavaScript");
+
+// Question 9
+var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(end);
