cbcf3094e32f7d946ba9221072be757d38469126
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-19 22:03:27 +0500

    chapter 5

--- books/a smarter way to learn javascript/chapter 5/app.js
+++ books/a smarter way to learn javascript/chapter 5/app.js
@@ -4,3 +4,7 @@
 // Question 2
 document.write("The sub of 3 and 5 is ", 3-5);
 document.write("<br>")
+document.write("The div of 3 and 5 is ", 3/5);
+document.write("<br>")
+document.write("The mul of 3 and 5 is ", 3*5);
+document.write("<br>")
