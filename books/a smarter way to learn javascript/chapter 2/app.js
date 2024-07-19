d660aed2971a7674e25c117054f6b9f65a5ff52e
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-19 21:12:37 +0500

    chapter 3

--- books/a smarter way to learn javascript/chapter 2/app.js
+++ books/a smarter way to learn javascript/chapter 2/app.js
@@ -1,36 +1,36 @@
 // Question 1
-var userName;
+// var userName;
 
 // Question 2
-var myName = "Naima Ahmad";
+// var myName = "Naima Ahmad";
 
 // Question 3
-var message;
-message = "Hello World!";
-alert(message);
+// var message;
+// message = "Hello World!";
+// alert(message);
 
 // Question 4
-var student = "Hammad";
-var age = "12 years old";
-var courseApplied = "Techno kids";
-alert(student);
-alert(age);
-alert(courseApplied);
+// var student = "Hammad";
+// var age = "12 years old";
+// var courseApplied = "Techno kids";
+// alert(student);
+// alert(age);
+// alert(courseApplied);
 
 // Question 5
-var item = "PIZZA\nPIZZ\nPIZ\nPI\nP"
-alert(item)
+// var item = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(item)
 
 // Question 6
-var email = "example@example.com";
-alert("My email address is " + email);
+// var email = "example@example.com";
+// alert("My email address is " + email);
 
 // Question 7
-var book = "A smarter way to learn JavaScript";
-alert("I am trying to learn from the Book " + book);
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
 
 // Question 8
-document.write("Yah! I can write HTML content throught JavaScript");
+// document.write("Yah! I can write HTML content throught JavaScript");
 
 // Question 9
 var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
---
+++ books/a smarter way to learn javascript/chapter 3/app.js
@@ -0,0 +1,20 @@
+// Question 1
+// var age;
+// age = 12;
+// alert("I am " + age + " years old");
+
+// Question 2
+// var track = 14;
+// alert(`You have visited this site ${track} times`);
+
+// Question 3
+// var birthYear = 2011;
+// document.write("My birth Year is "+birthYear);
+// document.write("")
+// document.write("Data type of my declared variable is number");
+
+// Question 4
+var visitorName = "Jawad";
+var product = "Laptop";
+var quantity = 2;
+document.write(visitorName + " wants to order " + quantity + " " + product + " on XYZ store");
