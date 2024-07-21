c22df13a831f868e3e5ef0d416158305825c4da6
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-21 16:23:54 +0500

    practice chapter 5

--- books/a smarter way to learn javascript/chapter 5/app.js
+++ books/a smarter way to learn javascript/chapter 5/app.js
@@ -2,9 +2,101 @@
 // document.write("The sum of 3 and 5 is ", 3+5);
 
 // Question 2
-document.write("The sub of 3 and 5 is ", 3-5);
+// document.write("The sub of 3 and 5 is ", 3-5);
+// document.write("<br>")
+// document.write("The div of 3 and 5 is ", 3/5);
+// document.write("<br>")
+// document.write("The mul of 3 and 5 is ", 3*5);
+// document.write("<br>")
+
+// Question 3
+// // a. Declare a variable.
+// var num;
+// // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// document.write("Value after variable declaration is: ??");
+// document.write("<br>")
+// // c. Initialize the variable with some number.
+// num = 5;
+// // d. Show the value of variable in your browser like “Initial value: 5”.
+// document.write("Initial value: "+num);
+// document.write("<br>")
+// // e. Increment the variable.
+// num = 6;
+// // f. Show the value of variable in your browser like “Value after increment is: 6”.
+// document.write("Value after increment is: "+num);
+// document.write("<br>")
+// // g. Add 7 to the variable.
+// num = num+7;
+// // h. Show the value of variable in your browser like “Value MATH EXPRESSIONS | JAVASCRIPT Page 2 of 9 after addition is: 13”.
+// document.write("Value of variable after addition is: "+num);
+// document.write("<br>")
+// // i. Decrement the variable.
+// num = 12;
+// // j. Show the value of variable in your browser like “Value after decrement is: 12”.
+// document.write("Value after decrement is: "+num);
+// document.write("<br>")
+// // k. Show the remainder after dividing the variable’s value by 3.
+// document.write("remainder after divinding the variable's value by 3 is: ",num/3) 
+// document.write("<br>")
+// // l. Output : “The remainder is : 0”.
+
+// Question 4
+// var ticktCost = 600;
+// var quantity = 5;
+// var total = ticktCost*quantity;
+// document.write("Total cost to buy "+quantity+" tickets to a movie is "+total+"PKR")
+
+// Question 5
+// document.write("Table of 5")
+// document.write("<br>")
+// document.write("5 x 1 = ",5*1)
+// document.write("<br>")
+// document.write("5 x 2 = ",5*2)
+// document.write("<br>")
+// document.write("5 x 3 = ",5*3)
+// document.write("<br>")
+// document.write("5 x 4 = ",5*4)
+// document.write("<br>")
+// document.write("5 x 5 = ",5*5)
+// document.write("<br>")
+// document.write("5 x 6 = ",5*6)
+// document.write("<br>")
+// document.write("5 x 7 = ",5*7)
+// document.write("<br>")
+// document.write("5 x 8 = ",5*8)
+// document.write("<br>")
+// document.write("5 x 9 = ",5*9)
+// document.write("<br>")
+// document.write("5 x 10 = ",5*10)
+
+// Question 6
+// var C = +prompt("Temperature in Celcius");
+// var fahrenheit = ((C*9/5)+32);
+// document.write(C+"℃ is "+fahrenheit+"℉");
+// document.write("<br>")
+// var F = +prompt("Temperature in Fahrenheit");
+// var celcius = ((F-32)*5/9)
+// document.write(F+"℉ is "+celcius+"℃")
+
+// Question 7
+var priceItem1 = 650;
+var priceItem2 = 100;
+var orderQuantityItem1 = 3;
+var orderQuantityItem2 = 7;
+var shippingCharges = 100;
+var price1 = 650*3;
+var price2 = 100*7;
+var totalCoast = price1+price2+shippingCharges;
 document.write("<br>")
-document.write("The div of 3 and 5 is ", 3/5);
+document.write("Price of item 1 is "+priceItem1);
 document.write("<br>")
-document.write("The mul of 3 and 5 is ", 3*5);
+document.write("Quantity of item 1 is "+orderQuantityItem1);
 document.write("<br>")
+document.write("Price of item 2 is "+priceItem2);
+document.write("<br>")
+document.write("Quantity of item 2 is "+orderQuantityItem2);
+document.write("<br>")
+document.write("Shipping Charges "+shippingCharges);
+document.write("<br>")
+document.write("<br>")
+document.write("Total cost of your order is "+totalCoast);
